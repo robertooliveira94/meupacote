e260f6ef44c6d6ec4c0dce4ef488dec63c7b78fa
roberto <robertotdeoliveira@hotmail.com>
2023-04-18 20:18:52 -0300

    Meu primeiro commit

---
+++ R/subtrair.R
@@ -0,0 +1,14 @@
+#'Função para subtrair
+#'
+#'Esta função serve para subtrair dois números
+#'
+#' @param a Um número
+#' @param b Um número
+#'
+#' @examples
+#' subtrair(3,4)
+#'
+#' @export
+subtrair <- function(a,b){
+  return(a-b)
+}
